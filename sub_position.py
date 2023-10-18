3cc85603ea61aaa22dad45b81fe22216eceafc6f
Yassine mazhare <137923652+Yassine-64@users.noreply.github.com>
2023-10-18 14:07:50 +0200

    Add files via upload

---
+++ sub_position.py
@@ -0,0 +1,4 @@
+chaine = input("entrer une chaine de caractere:")
+sous_chaine = input("entrer la sous_chaine:")
+position = chaine.find(sous_chaine)
+print("la position de la sous chaine est :", position)
